70a2149a0c500cbdf3641ffaf6b8df7107b10201
biu-biu <liuam007@users.noreply.github.com>
2019-02-19 10:37:04 +0800

    SwipeAction 可滑动单元格
    SwipeAction 可滑动单元格

---
+++ addrs.js
@@ -0,0 +1,51 @@
+Page({
+  data: {
+    swipeIndex: null,
+    list: [
+      { right: [{ type: 'edit', text: '取消收藏' },{ type: 'delete', text: '删除' },{ type: 'deletea', text: '删除' }], content: 'AAA' },
+      { right: [{ type: 'edit', text: '取消收藏' }, { type: 'delete', text: '删除' }], content: 'BBB' },
+      { right: [{ type: 'delete', text: '删除' }], content: 'CCC' },
+    ],
+  },
+  onRightItemClick(e) {
+    const { type } = e.detail;
+    my.confirm({
+      title: '温馨提示',
+      content: `${e.index}-${e.extra}-${JSON.stringify(e.detail)}`,
+      confirmButtonText: '确定',
+      cancelButtonText: '取消',
+      success: (result) => {
+        const { list } = this.data;
+        if (result.confirm) {
+          if(type === 'deletea'){
+            console.log(1);
+          }else if (type === 'delete') {
+            list.splice(this.data.swipeIndex, 1);
+            this.setData({
+              list: [...list],
+            });
+          }
+
+          my.showToast({
+            content: '确定 => 执行滑动删除还原',
+          });
+          e.done();
+        } else {
+          my.showToast({
+            content: '取消 => 滑动删除状态保持不变',
+          });
+        }
+      },
+    });
+  },
+  onItemClick(e) {
+    my.alert({
+      content: `dada${e.index}`,
+    });
+  },
+  onSwipeStart(e) {
+    this.setData({
+      swipeIndex: e.index,
+    });
+  },
+});
